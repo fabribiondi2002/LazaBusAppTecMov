114fe8c3d44687f3ae91efab504e6798327b9b2b
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-11-01 09:55:40 -0300

    add: lazabus icon

--- app/src/main/java/com/iua/gpi/lazabus/ui/screen/MainScreen.kt
+++ app/src/main/java/com/iua/gpi/lazabus/ui/screen/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -37,10 +38,10 @@ fun MainScreen() {
                 title = {
                     Row(verticalAlignment = Alignment.CenterVertically) {
                         Icon(
-                            imageVector = Icons.Filled.LocationOn,
+                            painter = painterResource(R.drawable.logo_white),
                             contentDescription = "Icono de Autobús",
                             tint = Color.White,
-                            modifier = Modifier.size(28.dp)
+                            modifier = Modifier.size(50.dp)
                         )
                         Spacer(Modifier.width(8.dp))
                         Text(
