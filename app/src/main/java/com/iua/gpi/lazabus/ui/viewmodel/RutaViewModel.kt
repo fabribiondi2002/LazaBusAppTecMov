e72586f057b9fbfd381f5ac3b1591dafdaeecf73
Biondi Leandro Fabrizio <lbiondi733@alumnos.iua.edu.ar>
2025-11-07 16:20:17 -0300

    Se agrego la funcionalidad de mantener la ruta optima en memoria, esta se aloja en RutaViewModel como rutaOptima

--- app/src/main/java/com/iua/gpi/lazabus/ui/viewmodel/RutaViewModel.kt
+++ app/src/main/java/com/iua/gpi/lazabus/ui/viewmodel/RutaViewModel.kt
@@ -1,33 +1,30 @@
 package com.iua.gpi.lazabus.ui.viewmodel
 
-
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.iua.gpi.lazabus.data.remote.model.Parada
 import com.iua.gpi.lazabus.data.remote.model.Ruta
 import com.iua.gpi.lazabus.data.remote.model.RutaOptima
 import com.iua.gpi.lazabus.data.remote.network.RetrofitClient
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import retrofit2.await
 
 class RutaViewModel : ViewModel() {
 
+    private val _rutaOptima = MutableStateFlow<RutaOptima?>(null)
+    val rutaOptima = _rutaOptima.asStateFlow()
 
-    var rutaOptima: RutaOptima? = null
-        private set
-    var rutas: List<Ruta> = emptyList()
-        private set
-    /**
-     * Calcular ruta óptima
-     */
+    private val _rutas = MutableStateFlow<List<Ruta>>(emptyList())
+    val rutas = _rutas.asStateFlow()
+
+
     fun calcularRutaOptima(
         olat: Double,
         olng: Double,
         dlat: Double,
         dlng: Double,
-        onResult: (RutaOptima) -> Unit,
         onError: (Throwable) -> Unit
     ) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -36,21 +33,20 @@ class RutaViewModel : ViewModel() {
                     .calcularRutaOptima(olat, olng, dlat, dlng)
                     .await()
 
-                rutaOptima = result
-                onResult(result)
-
+                _rutaOptima.value = result
             } catch (e: Exception) {
                 onError(e)
             }
         }
     }
 
+
     fun calcularRutas(
         olat: Double,
         olng: Double,
         dlat: Double,
         dlng: Double,
-        onResult: (List<Ruta>) -> Unit, onError: (Throwable) -> Unit
+        onError: (Throwable) -> Unit
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
@@ -58,12 +54,18 @@ class RutaViewModel : ViewModel() {
                     .calcularRutas(olat, olng, dlat, dlng)
                     .await()
 
-                rutas = result
-                onResult(result)
-
+                _rutas.value = result
             } catch (e: Exception) {
                 onError(e)
             }
         }
+    }
+
+
+    fun getRutaOptima(): RutaOptima? = rutaOptima.value
+
+    fun finalizarViaje() {
+        _rutaOptima.value = null
+        _rutas.value = emptyList()
     }
 }
